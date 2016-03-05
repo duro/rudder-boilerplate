e258b2ab12f96858fef571848c69404472be96fe
Adam Duro <adam@duromedia.com>
2016-03-05 14:16:47 -0500

    Update to latest ReduxAsyncConnect

--- frontend/src/ui/asyncList/AsyncList.js
+++ frontend/src/ui/asyncList/AsyncList.js
@@ -1,7 +1,15 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { loadList, isLoaded } from 'redux/modules/asyncList';
+import { asyncConnect } from 'redux-async-connect';
 
+@asyncConnect([{
+  promise: ({store: {dispatch, getState}}) => {
+    if (!isLoaded(getState())) {
+      return dispatch(loadList());
+    }
+  }
+}])
 @connect(
   state => ({
     listItems: state.asyncList.get('listItems')
@@ -14,12 +22,12 @@ export default class AsyncList extends Component {
     listItems: PropTypes.object
   }
 
-  static reduxAsyncConnect(params, store) {
-    const {dispatch, getState} = store;
-    if (!isLoaded(getState())) {
-      return dispatch(loadList());
-    }
-  }
+  // static reduxAsyncConnect(params, store) {
+  //   const {dispatch, getState} = store;
+  //   if (!isLoaded(getState())) {
+  //     return dispatch(loadList());
+  //   }
+  // }
 
   render() {
     const { listItems } = this.props;
